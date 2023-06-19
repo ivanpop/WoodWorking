0ca3309286444bdcd95ad4afecb42b4529e88525
Ivan Popov <unfragablegaming@gmail.com>
2023-06-19 21:49:35 +0300

    Initial commit

---
+++ Data/WoodWorkingDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Data
+{
+    public class WoodWorkingDbContext : IdentityDbContext
+    {
+        public WoodWorkingDbContext(DbContextOptions<WoodWorkingDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder
+            //    .Entity<Type>()
+            //    .HasData(new Type()
+            //    {
+            //        Id = 1,
+            //        Name = "Animals"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 2,
+            //        Name = "Fun"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 3,
+            //        Name = "Discussion"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 4,
+            //        Name = "Work"
+            //    });
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
